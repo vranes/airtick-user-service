d419b915f2f1e6596bd00dacc1709fd30fe805e4
vranes <nikolinavranes@gmail.com>
2020-12-30 00:22:33 +0100

    Initial commit

---
+++ src/main/java/com/raf/airtickuserservice/AirtickUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raf.airtickuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirtickUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirtickUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raf/airtickuserservice/AirtickUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.airtickuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirtickUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
